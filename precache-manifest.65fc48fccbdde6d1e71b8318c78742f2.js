ba223f560d6ef3092ebe4f668aa97a1ceac66fe2
suresh gunjewar <suru33378@gmail.com>
2021-06-19 16:18:38 +0530

    Updates

---
+++ precache-manifest.65fc48fccbdde6d1e71b8318c78742f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed268dbfa22d5b14a1493d3ce1fe980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5bc45f64fa609f88570",
+    "url": "/static/css/main.b697e07f.chunk.css"
+  },
+  {
+    "revision": "e992bafa1a93c23535d0",
+    "url": "/static/js/2.792e32ee.chunk.js"
+  },
+  {
+    "revision": "c5bc45f64fa609f88570",
+    "url": "/static/js/main.cb719257.chunk.js"
+  },
+  {
+    "revision": "9792b35d61958da63998",
+    "url": "/static/js/runtime~main.3bfa1488.js"
+  }
+]);
